9191dad77b49bab0303d164a80ed3f7d49d8d2dc
mariusmue <m.muench@bham.ac.uk>
2024-07-02 16:19:46 +0100

    S3: fix 2__run.sh to execute from the right directory

--- 1__setup/3__emulated_ue/2__run.sh
+++ 1__setup/3__emulated_ue/2__run.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
+
+readonly ROOT=$(realpath $(dirname "$0")/../..);
+readonly HERE=${ROOT}/1__setup/3__emulated_ue;
+
+pushd .;
+cd ${HERE};
+
 rm -rf ./log;
 mkdir log;
 
 docker run -v ./modem_files:/firmwire/modem_files -v ./log:/opt/setup/log  -v ./FirmWire:/opt/setup/firmwire --tty --interactive --rm simurai/s3:1.0.0;
+
+popd;
+exit 0;
