afadf40fb9a9c46a1839155e1655837f7b224aef
Tomasz Lisowski <t.p.lisowski@gmail.com>
2024-07-19 19:34:24 -0300

    Add note about how to restart phone in scrcpy

--- 1__setup/1__physical_ue/msg__nonet.sh
+++ 1__setup/1__physical_ue/msg__nonet.sh
@@ -4,5 +4,5 @@ readonly UTIL=""${ROOT}"/util";
 source ${UTIL}/color.sh;
 
 clear;
-printf "${CYEL}\\n\\nNetwork is running on a remote host.\\nPlease allow the different components to start, then you can proceed with using scrcpy to control the phones.\\nThe phone that has SIMurai attached to it, might need to be restarted before it will detect the SIM has been inserted. This is a side-effect of using SIMtrace2. Please be patient, it can take some time for the phone to connect to the cellular network and communicate with SIMurai.\\n${CDEF}";
+printf "${CYEL}\\n\\nNetwork is running on a remote host.\\nPlease allow the different components to start, then you can proceed with using scrcpy to control the phones.\\nThe phone that has SIMurai attached to it, might need to be restarted before it will detect the SIM has been inserted (in scrcpy hold \"alt\" and \"p\"). This is a side-effect of using SIMtrace2. Please be patient, it can take some time for the phone to connect to the cellular network and communicate with SIMurai.\\n${CDEF}";
 read -p "";
