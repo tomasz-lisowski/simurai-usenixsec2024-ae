f2f96652acf5d78ae3f34b0148fe2e9246aedec9
Marius Muench <m.muench@bham.ac.uk>
2024-10-27 08:14:39 +0000

    Allow S3 & E2 to run without attached phones by skipping ADB check

--- util/check_adb.sh
+++ util/check_adb.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
-adb start-server;
 
-# Make sure that server is up and devices had a chance to register 
-sleep 3;
+if [[ "${SIMURAI_SKIP_ADB_CHECK:-0}" -eq "1" ]]; then
+    printf "%sSkipping ADB check.%s\n" "${CCHK}" "${CDEF}";
+else
 
-check "If UE1 is connected and visible to ADB."
-adb -s "${UE1_SERIAL}" get-state;
-if ! checkResult "${?}" "UE1 is present." "Please plug in UE1. It is possible that you did not configure the serial number of UE1 correctly, take a look at \"util/adb_serial.sh\"."; then
-    exit 1;
+    adb start-server;
+
+    # Make sure that server is up and devices had a chance to register
+    sleep 3;
+
+    check "If UE1 is connected and visible to ADB."
+    adb -s "${UE1_SERIAL}" get-state;
+    if ! checkResult "${?}" "UE1 is present." "Please plug in UE1. It is possible that you did not configure the serial number of UE1 correctly, take a look at \"util/adb_serial.sh\"."; then
+        exit 1;
+    fi
+
+    check "If UE2 is connected and visible to ADB."
+    adb -s "${UE2_SERIAL}" get-state;
+    if ! checkResult "${?}" "UE2 is present." "Please plug in UE2. It is possible that you did not configure the serial number of UE2 correctly, take a look at \"util/adb_serial.sh\"."; then
+        exit 1;
+    fi
+
+    adb kill-server;
+
 fi
-
-check "If UE2 is connected and visible to ADB."
-adb -s "${UE2_SERIAL}" get-state;
-if ! checkResult "${?}" "UE2 is present." "Please plug in UE2. It is possible that you did not configure the serial number of UE2 correctly, take a look at \"util/adb_serial.sh\"."; then
-    exit 1;
-fi
-
-adb kill-server;
